035f5d0f7f64a05789e9f4d6d789043e45f05640
JBCODE <73259237+PiscoJaibey@users.noreply.github.com>
2021-09-11 21:50:33 -0500

    modificacion de studentctrl

--- gitbasics/src/main/java/com/tecsup/lab2/git/controller/StudentCtrl.java
+++ gitbasics/src/main/java/com/tecsup/lab2/git/controller/StudentCtrl.java
@@ -3,5 +3,6 @@ package com.tecsup.lab2.git.controller;
 public class StudentCtrl {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+	System.out.println("Esto es un cambio en StudentCtrl");
 	}
 }
